1626224802c8aaad8a510bfe136f271fd704481a
AconLight <w.mosieski@interia.pl>
2018-11-02 14:56:13 +0100

    properties meta data

--- TPA/Reflection/Model/PropertyMetaData.cs
+++ TPA/Reflection/Model/PropertyMetaData.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Reflection.Model
 {
-    class PropertyMetaData
+    internal class PropertyMetaData
     {
+
+        internal static IEnumerable<PropertyMetaData> EmitProperties(IEnumerable<PropertyInfo> props)
+        {
+            return from prop in props
+                   where prop.GetGetMethod().GetVisible() || prop.GetSetMethod().GetVisible()
+                   select new PropertyMetaData(prop.Name, TypeMetaData.EmitReference(prop.PropertyType));
+        }
+
+        #region private
+        private string m_Name;
+        private TypeMetaData m_TypeMetaData;
+        private PropertyMetaData(string propertyName, TypeMetaData propertyType)
+        {
+            m_Name = propertyName;
+            m_TypeMetaData = propertyType;
+        }
+        #endregion
+
     }
 }
