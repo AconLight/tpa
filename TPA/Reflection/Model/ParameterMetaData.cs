83978c252724e3f85746509857cf6b19c9a820f2
Wiktor <wiktor.muraszko@wp.pl>
2018-11-02 14:35:09 +0100

    ExtentionMethods/ParameterDate

--- TPA/Reflection/Model/ParameterMetaData.cs
+++ TPA/Reflection/Model/ParameterMetaData.cs
@@ -8,5 +8,14 @@ namespace Reflection.Model
 {
     class ParameterMetaData
     {
+        public ParameterMetadata(string name, TypeMetadata typeMetadata)
+        {
+            this.m_Name = name;
+            this.m_TypeMetadata = typeMetadata;
+        }
+
+        //private vars
+        private string m_Name;
+        private TypeMetadata m_TypeMetadata;
     }
 }
