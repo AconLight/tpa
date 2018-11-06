ea5eb5d140b79d5825a75fa376f8e1147343e999
Wiktor <wiktor.muraszko@wp.pl>
2018-11-07 00:19:42 +0100

    WPF
    Not working but not crushing neither. There is small chance but i can't load all obj at once

---
+++ TPA/WpfApp1/ModelTree/TreeViewItem.cs
@@ -0,0 +1,72 @@
+﻿using Reflection.Model;
+using Reflection.ModelTree;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class TreeViewItem
+    {
+        public ObservableCollection<TreeViewItem> Children { get; set; }
+        public TreeViewItem myself { get; set; }
+        public ModelNode whereAmI { get; set; }
+        public string Name { get; set; }
+        private bool m_IsExpanded;
+        private bool m_WasBuilt;
+        public Assembly assembly { get; set; }
+        public AssemblyMetaData assemblyMetadata { get; set; }
+        public ModelTreeHandler tree { get; set; }
+
+        public TreeViewItem(ModelNode me, String name)
+        {
+            Name = name;
+            Children = new ObservableCollection<TreeViewItem>();
+            foreach (var child in me.nodes)
+            {
+                Children.Add(new TreeViewItem(child, child.Name));
+            }
+            this.m_WasBuilt = false;
+            whereAmI = me;
+            
+        }
+        public TreeViewItem(String s)
+        {
+            Children = new ObservableCollection<TreeViewItem>();
+
+            assembly = Assembly.LoadFrom(s);
+            assemblyMetadata = new AssemblyMetaData(assembly);
+            tree = new ModelTreeHandler(assemblyMetadata);
+            this.m_WasBuilt = false;
+            Name = "(" + tree.currentNode.TypeName + ") " + tree.currentNode.Name;
+            whereAmI = tree.currentNode;
+
+        }
+        public bool IsExpanded
+        {
+            get { return m_IsExpanded; }
+            set
+            {
+                m_IsExpanded = value;
+                if (m_WasBuilt)
+                    return;
+                BuildMyself();
+
+                m_WasBuilt = true;
+            }
+        }
+        public void BuildMyself()
+        {
+            foreach(var child in whereAmI.nodes)
+            {
+                Children.Add(new TreeViewItem(child,child.Name));
+
+            }
+        }
+        
+    }
+}
--- TPA/WpfApp1/MyViewModel.cs
+++ TPA/WpfApp1/MyViewModel.cs
@@ -3,6 +3,7 @@ using Reflection.Model;
 using Reflection.ModelTree;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
@@ -16,7 +17,9 @@ namespace WpfApp1
     class MyViewModel : INotifyPropertyChanged
     {
         public RelayCommand Click_Browse { get; }
+        public ObservableCollection<TreeViewItem> HierarchicalAreas { get; set; }
         public RelayCommand Click_Load { get; }
+        public TreeViewItem root { get; set; }
         Assembly assembly { get; set; }
         public Visibility ChangeControlVisibility { get; set; } = Visibility.Hidden;
         public string pathVariable { get; set; }
@@ -25,7 +28,7 @@ namespace WpfApp1
         {
             Click_Browse = new RelayCommand(Browse);
             Click_Load = new RelayCommand(Load);
-
+            HierarchicalAreas = new ObservableCollection<TreeViewItem>();
         }
         public event PropertyChangedEventHandler PropertyChanged;
         private void RaisePropertyChanged(string propertyName_)
@@ -52,7 +55,17 @@ namespace WpfApp1
         }
         private void Load()
         {
-            assembly = Assembly.LoadFrom(pathVariable);
+            if (pathVariable.Substring(pathVariable.Length - 4) == ".dll")
+            {
+                //assembly = Assembly.LoadFrom(pathVariable);
+                root = new TreeViewItem(pathVariable);
+                
+                HierarchicalAreas.Add(root);
+
+                RaisePropertyChanged(nameof(HierarchicalAreas));
+
+
+            }
         }
     }
 }
