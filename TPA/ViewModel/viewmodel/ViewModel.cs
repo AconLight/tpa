67ee25cd14c74e461b1fe57a07362f22cfe261c6
AconLight <w.mosieski@interia.pl>
2018-11-14 19:38:39 +0100

    new structure of the project

---
+++ TPA/ViewModel/viewmodel/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Reflection.ModelTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.viewmodel
+{
+    class ViewModel : ViewModelInterface
+    {
+        public ModelTreeHandler tree;
+        public void CloseNode(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenNode(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TPA/ViewModel/viewmodel/ViewModelInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.viewmodel
+{
+    public interface ViewModelInterface
+    {
+        void OpenNode(int id);
+        void CloseNode(int id);
+    }
+}
+Class1.cs
