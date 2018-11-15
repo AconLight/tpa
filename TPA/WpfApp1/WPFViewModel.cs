bb8b15bc72a97f2326459ddd3c78a732fcc71ac7
Wiktor <wiktor.muraszko@wp.pl>
2018-11-15 14:49:57 +0100

    czyszczenie

--- TPA/WpfApp1/WPFViewModel.cs
+++ TPA/WpfApp1/WPFViewModel.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using ViewModel.viewmodel;
 
