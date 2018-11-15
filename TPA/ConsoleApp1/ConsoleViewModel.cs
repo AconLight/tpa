eff2949382d76ea74bb1a40d7964b2ba6158da40
AconLight <w.mosieski@interia.pl>
2018-11-15 14:28:50 +0100

    juz nie moge z nim

--- TPA/ConsoleApp1/ConsoleViewModel.cs
+++ TPA/ConsoleApp1/ConsoleViewModel.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewModel.viewmodel;
 
 namespace ConsoleApp1
 {
-    class ConsoleViewModel
+    class ConsoleViewModel : ViewModelClass
     {
+        public override void Browse()
+        {
+            String p = Console.ReadLine();
+            pathVariable = p;
+        }
     }
 }
