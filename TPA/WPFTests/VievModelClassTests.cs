be811376f79b39739bc25f222c70b3c32f7283d3
Wiktor <wiktor.muraszko@wp.pl>
2019-01-02 21:48:05 +0100

    VievmodelClassTests

---
+++ TPA/WPFTests/VievModelClassTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViewModel.viewmodel;
+
+namespace PresentationTests
+{
+    [TestClass]
+    public class VievModelClassTests
+    {
+        private ViewModelClass viewModelClass;
+        [TestInitialize]
+        public void Initialize()
+        {
+            viewModelClass = new ViewModelClass(null);
+        }
+        [TestMethod]
+        public void pathVariable_WhenModified_ValueSchouldChange()
+        {
+            string oldOne = "oldOne";
+            string newOne = "newOne";
+
+            viewModelClass.pathVariable = oldOne;
+            Assert.AreEqual(oldOne, viewModelClass.pathVariable);
+
+            viewModelClass.pathVariable = newOne;
+            Assert.AreEqual(newOne, viewModelClass.pathVariable);
+
+        }
+        [TestMethod]
+        public void load_WhenNotDllFileisChosen_ShouldBeOk()
+        {
+            viewModelClass.Load();
+            Assert.IsNull(viewModelClass.root);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void load_WhenWrongPathIsChosen_ShouldThrowExeption()
+        {
+            viewModelClass.pathVariable = "Wrong.dll";
+            viewModelClass.Load();
+        }
+        
+    }
+}
