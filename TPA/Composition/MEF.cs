8c0c879dbc96d5332d8165cb743fc5a10c702909
Wiktor <wiktor.muraszko@wp.pl>
2019-01-03 21:12:43 +0100

    socie

---
+++ TPA/Composition/MEF.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+
+
+namespace Composition
+{
+    public class MEF
+    {
+        public CompositionContainer compositionContainer;
+        public MEF()
+        {
+
+        }
+    }
+}
