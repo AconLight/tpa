96e1edfe86b4eec1dd5f86e7da5564dff9b5394d
AconLight <w.mosieski@interia.pl>
2019-01-17 19:53:15 +0100

    elo

--- TPA/Database/DBHandler.cs
+++ TPA/Database/DBHandler.cs
@@ -78,10 +78,74 @@ namespace Database
             DBModelNode newNode = null;
             foreach (LogicModelNode child in node.allNodes)
             {
-                myNode.children.Add(newNode = cDBModelNode(node.TypeName, node.Name, myNode));
+                myNode.children.Add(newNode = cDBModelNode(child.TypeName, child.Name, myNode));
                 loadedNodes.Add(myNode);
                 TreeSeekToDB(newNode, child, loadedNodes);
             }
+        }
+
+        public static LogicModelTreeHandler createLogicModel(List<DBModelNode> nodes)
+        {
+            LogicModelTreeHandler modelTree = new LogicModelTreeHandler();
+            LogicModelNodeAssembly root = null;
+            DBModelNode dbRoot = null;
+            foreach (DBModelNode node in nodes)
+            {
+                if (node.Parent == null)
+                {
+                    dbRoot = node;
+                    root = new LogicModelNodeAssembly(null, node.Name);
+                    break;
+                }
+            }
+            modelTree.rootNode = root;
+            modelTree.currentNode = root;
+
+            List<LogicModelNode> loadedNodes = new List<LogicModelNode>();
+
+            TreeSeekToLogic(root, dbRoot, loadedNodes);
+
+            return modelTree;
+        }
+
+        private static void TreeSeekToLogic(LogicModelNode myNode, DBModelNode node, List<LogicModelNode> loadedNodes)
+        {
+            LogicModelNode newNode = null;
+            foreach (DBModelNode child in node.children)
+            {
+                if (child.TypeName.Contains("Method"))
+                {
+                    myNode.allNodes.Add(newNode = new LogicModelNodeMethod(myNode, child.Name));
+                }
+                else if (child.TypeName.Contains("Namespace"))
+                {
+                    myNode.allNodes.Add(newNode = new LogicModelNodeNamespace(myNode, child.Name));
+                }
+                else if (child.TypeName.Contains("Type") || child.TypeName.Contains("Property")
+                    || child.TypeName.Contains("Interface") || child.TypeName.Contains("Nested Type")
+                    || child.TypeName.Contains("Base Type") || child.TypeName.Contains("Declaring Type")
+                    || child.TypeName.Contains("Return Type") || child.TypeName.Contains("Parameter Type"))
+                {
+                    myNode.allNodes.Add(newNode = new LogicModelNodeType(myNode, child.Name, child.TypeName));
+                }
+                else
+                {
+                    return;
+                }
+                
+
+                foreach (LogicModelNode vn in loadedNodes)
+                {
+                    if (vn.TypeName == newNode.TypeName && vn.Name == newNode.Name)
+                    {
+                        newNode.isLooped = true;
+                        break;
+                    }
+                }
+                if(!newNode.isLooped)
+                    TreeSeekToLogic(newNode, child, loadedNodes);
+            }
+            myNode.loadNodes();
         }
 
 
